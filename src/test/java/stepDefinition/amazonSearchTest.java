6a002aadc137814e9e545d2ca0c369b528d323c8
PROJITA KAR <projita2003kar@gmail.com>
2026-03-12 15:19:27 +0530

    Added Amazon cart flow using BDD

--- src/test/java/stepDefinition/amazonSearchTest.java
+++ src/test/java/stepDefinition/amazonSearchTest.java
@@ -14,7 +14,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class amazonLoginTest {
+public class amazonSearchTest {
 	WebDriver driver;
 	String parent;
 	@Given("User is on the home page")
